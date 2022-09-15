494f0471dd7099077a844c405ef5a232500348cb
Snehith Bootla <snehithbootla123@gmail.com>
2022-09-15 15:51:42 -0400

    Removed static file folder

--- Quickart_API/Program.cs
+++ Quickart_API/Program.cs
@@ -19,12 +19,7 @@ if (app.Environment.IsDevelopment())
 } 
 
 app.UseHttpsRedirection();
-app.UseStaticFiles(new StaticFileOptions
-{
-    FileProvider = new PhysicalFileProvider(
-           Path.Combine(builder.Environment.ContentRootPath, "MyStaticFiles")),
-    RequestPath = "/StaticFiles"
-});
+
 
 app.UseAuthorization();
 
